431363b8d991aedccb753dd110426bfe1a96e212
Alaysia Veal <alaysiaveal@gmail.com>
2022-12-09 14:32:24 -0500

    completed auth

--- middleware/index.js
+++ middleware/index.js
@@ -1,3 +1,56 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const { has } = require('lodash')
 require('dotenv').config()
+
+const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
+const APP_KEY = process.env.APP_KEY
+
+const hashPassword = async (password) => {
+  let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+  return hashedPassword
+}
+
+const comparePassword = async (storedPassword, password) => {
+  let passwordMatch = await bcrypt.compare(password, storedPassword)
+  return passwordMatch
+}
+
+const Token = (payload) => {
+  let token = jwt.sign(payload, APP_KEY)
+  return token
+}
+
+const checkToken = (req, res, next) => {
+  const { token } = res.locals
+  try {
+    let payload = jwt.verify(token, APP_KEY)
+    if (payload) {
+      res.locals.payload = payload
+      return next()
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  }
+}
+
+const stripToken = (req, res, next) => {
+  try {
+    const token = req.headers['authorization'].split(' ')[1]
+    if (token) {
+      res.locals.token = token
+      return next()
+    }
+  } catch (error) {
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  }
+}
+
+module.exports = {
+  stripToken,
+  checkToken,
+  Token,
+  comparePassword,
+  hashPassword
+}
